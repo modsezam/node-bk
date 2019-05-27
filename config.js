791ea329857d233c5d5ab96bdbd0b85b80351c4d
marcin.kobierowski <modsezam@gmail.com>
2019-05-27 22:47:05 +0200

    change config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:FTlnSlV8qBQHh4AE@cluster0-g2kzr.mongodb.net/test?=true',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
