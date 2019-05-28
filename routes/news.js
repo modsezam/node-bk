ac74a3147703c271375d3ab7a32cb60421c317ba
marcin.kobierowski <modsezam@gmail.com>
2019-05-28 20:32:05 +0200

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('news', { title: 'News' });
+    //console.log(req.query.search);
+    const search = req.query.search || '';
+
+    const findNews = News
+    .find({title: new RegExp(search.trim(), 'i')})
+    .sort({created: -1});
+
+
+    findNews.exec((err, data) => {
+        //console.log(data)
+        res.render('news', { title: 'News', data, search });
+    });
 });
 
 module.exports = router;
